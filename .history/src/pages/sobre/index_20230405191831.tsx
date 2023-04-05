5ab1d37b97af4c5b81178fe932c65ce8f7a5cf2c
weltergab <weltergab@gmail.com>
2023-04-05 19:34:20 -0300

    copia dos arquivos - routes

---
+++ .history/src/pages/sobre/index_20230405191831.tsx
@@ -0,0 +1,5 @@
+export default function Sobre(){
+    return <div>
+        <h1>Pagina Sobre</h1>
+    </div>
+}
---
+++ .history/src/pages/sobre/index_20230405192254.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+export default function Sobre(){
+    return <div>
+        <h1>Pagina Sobre</h1>
+
+        <Link href="/">home</Link>
+    </div>
+}
